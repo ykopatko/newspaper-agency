7854826ac0e1ec9a70233f7ca52448f65e12bc45
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-03-30 13:21:18 +0300

    Create models Topic, Newspaper, Redactor, make migrations and update settings

--- agency/models.py
+++ agency/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from newspaper_agency import settings
+# from django.urls import reverse # will use later
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    context = models.TextField()
+    published_date = models.DateField()
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    redactors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    def __str__(self):
+        return self.title
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField()
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+    # def get_absolute_url(self):
+    #     return reverse("agency:redactor-detail", kwargs={"pk": self.pk})
+    # will add later
--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# AUTH_USER_MODEL = "" # will add later
+AUTH_USER_MODEL = "agency.Redactor"
 
 LOGIN_REDIRECT_URL = "/"
 
